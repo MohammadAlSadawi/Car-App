bbd0de3d0db3841d071a27b982d1106f01c21dd1
MohammadAlSadawi <117226366+MohammadAlSadawi@users.noreply.github.com>
2026-04-18 18:53:57 +0300

    Setup project structure and Add models and ApiResponse structure

--- WebCarApp/Services/ICarService.cs
+++ WebCarApp/Services/ICarService.cs
@@ -6,6 +6,6 @@ namespace WebCarApp.Services
     {
         Task<List<Make>> GetAllMakes();
         Task<List<VehicleType>> GetVehicleTypesByMakedId(int makeId);
-        Task<List<CarModels>> GetModels(int makeId, int year);
+        Task<List<CarModels>> GetModelsByMakedIdAndYear(int makeId, int year);
     }
 }
